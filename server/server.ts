a34d1983bd30704f3528662da68575ede90d7164
Keith Simmons <keith@the-simmons.net>
2018-03-28 21:49:50 -0700

    some progress

--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,85 @@
-import {app, BrowserWindow} from "electron";
+import {app, BrowserWindow, screen, ipcMain as ipc, globalShortcut} from "electron";
 import * as path from "path";
 import * as url from "url";
 
+import { spawn } from 'child_process';
+import * as rl from 'readline';
+
+var keyboardProcess = spawn(path.resolve(path.join(__dirname, '../KeyboardState.exe')));
+var keyboardOutput = rl.createInterface({ input: keyboardProcess.stdout, terminal: false });
+
+function readLine() {
+  return new Promise<string>((resolve, reject) => {
+    keyboardOutput.once('line', (text) => {
+      resolve(text as string);
+    });
+  })
+}
+
+async function keyDown(key: string) {
+  keyboardProcess.stdin.write(key);
+  var result = await readLine();
+  if (result.trim() == "True") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 let win;
+let timeout: any = null;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
-
-  win.loadURL(url.format({
-    pathname: path.resolve(__dirname, "pip", "pip.html"),
-    protocol: 'file:',
-    slashes: true
-  }));
-
-  win.webContents.openDevTools();
-
-  win.on('closed', () => {
-    win = null;
+  let preloadPath = path.join(__dirname, "pip/main.js");
+  win = new BrowserWindow({
+    skipTaskbar: true,
+    transparent: true,
+    alwaysOnTop: true,
+    frame: false,
+    thickFrame: true,
+    show: false,
+    webPreferences: {
+      preload: preloadPath
+    }
   });
+
+  win.once('ready-to-show', () => {
+    console.log("pip ready");
+    win.show();
+
+    let display = screen.getDisplayNearestPoint(screen.getCursorScreenPoint());
+    let width = Math.floor(display.bounds.width * 0.25);
+    let height = Math.floor(width * 0.5625);
+    let bounds = {
+      x: display.bounds.x + display.bounds.width - width - 25,
+      y: display.bounds.y + display.bounds.height - height - 65,
+      width: width, height: height
+    };
+    win.setContentBounds(bounds);
+    win.webContents.send("mouseMoved", {x: -1000000, y: -1000000});
+  });
+
+  timeout = setInterval(async () => {
+    var altDown = await keyDown("ControlKey\n");
+    win.setIgnoreMouseEvents(!altDown);
+    var cursorScreenPoint = screen.getCursorScreenPoint();
+    var windowBounds = win.getContentBounds();
+    cursorScreenPoint.x -= windowBounds.x;
+    cursorScreenPoint.y -= windowBounds.y;
+    if (!altDown &&
+        cursorScreenPoint.x > -250 && cursorScreenPoint.x < windowBounds.width + 250 &&
+        cursorScreenPoint.y > -250 && cursorScreenPoint.y < windowBounds.height + 250) {
+      win.webContents.send("mouseMoved", cursorScreenPoint);
+    } else {
+      win.webContents.send("mouseMoved", {x: -100000, y: -10000});
+    }
+  }, 16);
+
+  win.loadURL(process.argv[2]);
 }
+
+ipc.on('errorInWindow', (event: any, data: any) => {
+  console.log(data);
+});
 
 app.on('ready', createWindow);
